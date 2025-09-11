2cc8431bdff4be3c3facbe26a1e219d23f7f8620
AhamedJazira-M <jazirababy23@gmail.com>
2025-09-11 21:34:10 +0530

    created

--- Matplotlib/Matplotlib.py
+++ Matplotlib/Matplotlib.py
@@ -2,5 +2,17 @@
 #Add ! before pip to install in google colab or jupyter notebook
 
 #pip install matplotlib
+#pip install numpy
 
 import matplotlib.pyplot as plt
+import numpy as np
+
+#BASIC PLOT
+
+x=[1,2,3,4,5,6,7,8,9]
+y=[10,2,8,3,9,6,4,5,8]
+plt.plot(x,y)
+plt.show()
+
+#MARKER
+
