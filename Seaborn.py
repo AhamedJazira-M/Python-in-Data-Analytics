97b580e7d612c2d61eb93a763d060a7a7882cea2
Ahamed Jazira M <jazirababy23@gmail.com>
2025-09-19 20:30:48 +0530

    Updated Seaborn.py

--- Seaborn.py
+++ Seaborn.py
@@ -19,4 +19,25 @@ sns.replot(x="hours",y="marks",hue="age",data=dataset,kind="line",style="interne
 #Data Distribution 
 
 #Histogram
-sns.displot(hsj
+sns.displot(dataset, x="age", binwidth=0.5,hue="internet",multiple="stack")
+#bins ---> Groups the bars or columns in the chart as given.
+sns.displot(dataset, x="marks",col="internet")
+
+#Bivariate Distribution 
+sns.displot(dataset, x="age", y="marks",hue="internet")
+
+#kernel Density Estimation KDE
+kind="kde"
+
+#Pair Plot
+sns.pairplot(dataset)
+
+#Categorical Data
+
+#Scatter Plot
+sns.catplot(x,y,data,hue)
+sns.catplot(x,y,kind=box,data,hue)
+
+#Regression
+sns.replot(x,y,data)
+sns.lmplot(x,y,data,hue,markers=['o','x'],palette='Set1')
